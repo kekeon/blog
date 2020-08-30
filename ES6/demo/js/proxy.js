a86a6c29116addc006be70249c1aa4d13a4786e9
kekeon <liuke095@gmail.com>
2020-08-31 00:00:22 +0800

    update proxy

--- ES6/demo/js/proxy.js
+++ ES6/demo/js/proxy.js
@@ -1,17 +1,23 @@
 (function () {
-    console.log("%c proxy start", "color: red;")
 
-    let obj = new Proxy({}, {
-        get: function (target, propKey, receiver) {
-            console.log(`getting ${propKey}!`, target, receiver);
-            return Reflect.get(target, propKey, receiver);
-        },
-        set: function (target, propKey, value, receiver) {
-            console.log(`setting ${propKey}!`);
-            return Reflect.set(target, propKey, value, receiver);
-        }
-    });
 
-    ++obj.count
+  let p = new Proxy({}, {
+    get: function (target, p, receiver) {
+      console.log('target, p, get', target, p, receiver)
+      return target[p]
+    },
+    set: function (target, p, value) {
+      console.log('target, p, set', target, p, value)
+      target[p] = value
+    },
+    has: function (target, propKey) {
+      console.log('target, p, hasOwnProperty', target, propKey)
+      return Reflect.has(target, propKey)
+    }
+  });
 
-})()
+  console.log(p.name);
+  console.log(p.name = 1);
+  console.log('name' in p);
+
+})();
